942dd4cba39518d6256ce6f65bfcdc0bf06ed8e0
George Chokhonelidze <g_chokhonelidze@outlook.com>
2022-06-06 19:52:55 -0500

    moving env to env

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const DB = process.env.DB
   ? process.env.DB
-  : "mongodb://admin:cst10b002944@localhost:27017/mydb?retryWrites=true&w=majority";
+  : "mongodb://mongo:27017/app_development";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
