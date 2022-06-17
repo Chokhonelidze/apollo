8a647db457bd852b6fe98db3522d45d185dbbbb6
George Chokhonelidze <g_chokhonelidze@outlook.com>
2022-06-16 20:39:45 -0500

    last try

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const DotenvPlugin = require("dotenv-webpack");
-
-module.exports = {
-  entry: "./index.js",
-  plugins: [new DotenvPlugin({ systemvars: true })],
-};
